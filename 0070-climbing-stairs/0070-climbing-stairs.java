b056d891f5d4a20b91b13fd49b6b9dea8a004a97
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2022-12-24 12:46:45 +0530

    Time: 0 ms (100.00%), Space: 39 MB (89.84%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,15 +3,15 @@ class Solution {
     public int climbStairs(int n) {
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        return findSolution(n,dp);
+        return solution(n,dp);
     }
-    int findSolution(int n,int[] dp){
-        if(n==0) return 1;
-        if(n==1) return 1;
+    int solution(int ind,int dp[]){
+        if(ind==0) return 1;
+        if(ind==1) return 1;
+        if(dp[ind]!=-1) return dp[ind];
+        int left=solution(ind-1,dp);
+        int right=solution(ind-2,dp);
         
-        if(dp[n]!=-1) return dp[n];
-        int left=findSolution(n-1,dp);
-        int right=findSolution(n-2,dp);
-        return dp[n]=left+right;
+        return dp[ind]=left+right;
     }
 }
