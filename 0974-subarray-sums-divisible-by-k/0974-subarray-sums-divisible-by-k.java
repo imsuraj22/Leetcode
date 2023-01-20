beb50628fa03ac13e44470a196fd8a8b56fdfb45
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-01-20 15:24:41 +0530

    Time: 4 ms (92.70%), Space: 45.9 MB (94.37%) - LeetHub

--- 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -1,14 +1,15 @@
 class Solution {
      public int subarraysDivByK(int[] nums, int k) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0, remainder; i < nums.length; i++) {
-            if (i > 0) nums[i] += nums[i - 1];
-            remainder = (nums[i] % k + k) % k;
-            map.put(remainder, map.getOrDefault(remainder, 0) + 1);
+         int[] modCount = new int[k];
+    modCount[0] = 1;
+    int sum = 0, count = 0;
+    for (int num : nums) {
+        sum = (sum + num) % k;
+        if (sum < 0) {
+            sum += k;
         }
-        int result = map.getOrDefault(0, 0);
-        for (int frequency : map.values())
-            result += frequency * (frequency - 1) / 2;
-        return result;
+        count += modCount[sum]++;
+    }
+    return count;
     }
 }
