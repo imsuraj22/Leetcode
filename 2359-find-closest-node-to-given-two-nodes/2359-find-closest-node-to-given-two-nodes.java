3739c0da1138b5ff911250b8e4f4cd4db66f732e
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-01-25 23:26:39 +0530

    Time: 23 ms (68.26%), Space: 106.9 MB (29.94%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.java
@@ -1,36 +1,47 @@
 class Solution {
-     public void dfs(int node, int[] edges, int[] dist, Boolean[] visit) {
-        visit[node] = true;
-        int neighbor = edges[node];
-        if (neighbor != -1 && !visit[neighbor]) {
-            dist[neighbor] = 1 + dist[node];
-            dfs(neighbor, edges, dist, visit);
-        }
-    }
-
     public int closestMeetingNode(int[] edges, int node1, int node2) {
-        int n = edges.length;
-        int[] dist1 = new int[n], dist2 = new int[n];
-        Arrays.fill(dist1, Integer.MAX_VALUE);
-        Arrays.fill(dist2, Integer.MAX_VALUE);
-        dist1[node1] = 0;
-        dist2[node2] = 0;
-
-        Boolean[] visit1 = new Boolean[n], visit2 = new Boolean[n];
-        Arrays.fill(visit1, Boolean.FALSE);
-        Arrays.fill(visit2, Boolean.FALSE);
-
-        dfs(node1, edges, dist1, visit1);
-        dfs(node2, edges, dist2, visit2);
-
-        int minDistNode = -1, minDistTillNow = Integer.MAX_VALUE;
-        for (int currNode = 0; currNode < n; currNode++) {
-            if (minDistTillNow > Math.max(dist1[currNode], dist2[currNode])) {
-                minDistNode = currNode;
-                minDistTillNow = Math.max(dist1[currNode], dist2[currNode]);
+        int n=edges.length;
+        
+        int dist1[]=new int[n];
+        int dist2[]=new int[n];
+        Arrays.fill(dist1,-1);
+        Arrays.fill(dist2,-1);
+        int d=1;
+        
+        while(node1!=-1){
+            
+            if(dist1[node1]!=-1){
+                break;
+            }
+            
+            dist1[node1]+=d;
+            node1=edges[node1];
+            d++;
+        }
+        
+        d=1;
+        
+         while(node2!=-1){
+            
+            if(dist2[node2]!=-1){
+                break;
+            }
+            
+            dist2[node2]+=d;
+            node2=edges[node2];
+            d++;
+        }
+        
+        int minD=Integer.MAX_VALUE;
+        int ans=-1;
+        for(int i=0;i<n;i++){
+            if(dist1[i]!=-1 && dist2[i]!=-1){
+                if(minD>Math.max(dist1[i],dist2[i])){
+                    minD=Math.max(dist1[i],dist2[i]);
+                    ans=i;
+                }
             }
         }
-
-        return minDistNode;
+        return ans;
     }
 }
