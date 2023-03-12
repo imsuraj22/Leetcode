f56bf97cd48b3d4f2f277d7053d0c2aaa4d52303
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-03-12 13:32:23 +0530

    Time: 3 ms (84.23%), Space: 44.5 MB (27.41%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -9,28 +9,32 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] lists) {
-        if(lists.length==0) return null;
-        ListNode list1=lists[0];
-        //ListNode ans=null;
-        for(int i=1;i<lists.length;i++){
-          list1=mergeTwoLists(list1,lists[i]);
-        }
+    public static ListNode mergeKLists(ListNode[] lists){
+    return partion(lists,0,lists.length-1);
+}
 
-        return list1;
-  }
+public static ListNode partion(ListNode[] lists,int s,int e){
+    if(s==e)  return lists[s];
+    if(s<e){
+        int q=(s+e)/2;
+        ListNode l1=partion(lists,s,q);
+        ListNode l2=partion(lists,q+1,e);
+        return merge(l1,l2);
+    }else
+        return null;
+}
 
-  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-    if(list1==null) return list2;
-    if(list2==null) return list1;
-        
-    if(list1.val<=list2.val){
-      list1.next=mergeTwoLists(list1.next, list2);
-      return list1;
+//This function is from Merge Two Sorted Lists.
+public static ListNode merge(ListNode l1,ListNode l2){
+    if(l1==null) return l2;
+    if(l2==null) return l1;
+    if(l1.val<l2.val){
+        l1.next=merge(l1.next,l2);
+        return l1;
+    }else{
+        l2.next=merge(l1,l2.next);
+        return l2;
     }
-    else{
-      list2.next=mergeTwoLists(list1, list2.next);
-      return list2;
-    }
-  }
+}
+  
 }
