c9f8b31800cb418957d1dc6d39bb13f38bcd8cb7
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-06-04 10:15:29 +0530

    Time: 3 ms (34.77%), Space: 44.4 MB (7.22%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -0,0 +1,39 @@
+class Solution {
+  
+        public int findCircleNum(int[][] isConnected) {
+     
+        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
+        int n=isConnected.length;
+        for(int i=0;i<=n;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<isConnected[0].length;j++){
+                if(isConnected[i][j]==1 && i!=j){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
+                }
+            }
+        }
+
+        int count=0;
+        int vis[]=new int[n];
+        for(int i=0;i<n;i++){
+            if(vis[i]!=1){
+                dfs(i, adj, vis);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    void dfs(int i,ArrayList<ArrayList<Integer>> adj, int vis[] ){
+        vis[i]=1;
+        for(Integer it:adj.get(i)){
+            if(vis[it]==0){
+                dfs(it, adj, vis);
+            }
+        }
+    }
+}
