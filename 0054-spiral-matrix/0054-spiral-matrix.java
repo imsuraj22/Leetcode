ced58806279fa7fc553d035cd0efc4944c35acd9
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-06-05 12:42:55 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (83.61%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,31 +1,31 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<Integer>();
-        if(matrix.length == 0 || matrix[0].length == 0) return res;
-        
-        int top = 0;
-        int bottom = matrix.length-1;
-        int left = 0;
-        int right = matrix[0].length-1;
-        
-        while(true){
-            for(int i = left; i <= right; i++) res.add(matrix[top][i]);
+         List<Integer> list=new ArrayList<>();
+         if(matrix.length == 0 || matrix[0].length == 0) return list;
+         int top=0;
+         int bottom=matrix.length-1;
+         int left=0;
+         int right=matrix[0].length-1;
+
+
+         while(true){
+            for(int i=left;i<=right;i++) list.add(matrix[top][i]);
             top++;
-            if(left > right || top > bottom) break;
-            
-            for(int i = top; i <= bottom; i++) res.add(matrix[i][right]);
+            if(top>bottom || left>right) break;
+
+            for(int i=top;i<=bottom;i++) list.add(matrix[i][right]);
             right--;
-            if(left > right || top > bottom) break;
-            
-            for(int i = right; i >= left; i--) res.add(matrix[bottom][i]);
+            if(top>bottom || left>right) break;
+
+            for(int i=right;i>=left;i--) list.add(matrix[bottom][i]);
             bottom--;
-            if(left > right || top > bottom) break;
-            
-            for(int i = bottom; i >= top; i--) res.add(matrix[i][left]);
+            if(top>bottom || left>right) break;
+
+            for(int i=bottom;i>=top;i--) list.add(matrix[i][left]);
             left++;
-            if(left > right || top > bottom) break;
-        }
-        
-        return res;
+            if(top>bottom || left>right) break;
+         }
+
+         return list;
     }
 }
