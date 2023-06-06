fee0fdcf06385d96d3d3de9d3e8513dc3dcd37da
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-06-06 12:57:03 +0530

    Time: 1 ms (97.47%), Space: 44.2 MB (87.87%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.java
+++ 0057-insert-interval/0057-insert-interval.java
@@ -1,25 +1,17 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-       ArrayList<int[]> list=new ArrayList<>();
+        ArrayList<int[]> list=new ArrayList<>();
         for(int i=0;i<intervals.length;i++){
-
             if(intervals[i][1]<newInterval[0]) list.add(intervals[i]);
             else if(intervals[i][0]>newInterval[1]){
                 list.add(newInterval);
                 newInterval=intervals[i];
+            }else{
+                newInterval[0]=Math.min(newInterval[0], intervals[i][0]);
+                newInterval[1]=Math.max(newInterval[1], intervals[i][1]);
             }
-            else{
-                newInterval[0]=Math.min(intervals[i][0], newInterval[0]);
-                newInterval[1]=Math.max(intervals[i][1], newInterval[1]);
-            }
-
         }
         list.add(newInterval);
-        int ans[][]=new int[list.size()][2];
-        for(int i=0;i<list.size();i++){
-            ans[i]=list.get(i);
-        }
-
-        return ans;
+        return list.toArray(new int[0][]);
     }
 }
