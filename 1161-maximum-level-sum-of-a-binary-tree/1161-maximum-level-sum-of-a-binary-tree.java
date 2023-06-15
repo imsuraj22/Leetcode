edb9b8167e1016ed24968a79355464bd83fb43ac
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-06-15 21:19:54 +0530

    Time: 9 ms (61.46%), Space: 46.8 MB (7.50%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.java
@@ -15,32 +15,15 @@
  */
 class Solution {
     public int maxLevelSum(TreeNode root) {
-          Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        int level=0;
-        int max=Integer.MIN_VALUE;
-        int flevel=0;
-        while(!q.isEmpty()){
-            int size=q.size();
-            int curr=0;
-
-            for(int i=0;i<size;i++){
-                TreeNode c=q.poll();
-                curr+=c.val;
-                if(c.left!=null){
-                    q.add(c.left);
-
-                }
-                if(c.right!=null){
-                    q.add(c.right);
-                }
-            }
-            level++;
-            if(curr>max) {
-                max=curr;
-                flevel=level;
-            }
-        }
-        return flevel;
+        List<Integer> list = new ArrayList<>();
+        dfs(root, list, 0);
+        return 1 + IntStream.range(0, list.size()).reduce(0, (a, b) -> list.get(a) < list.get(b) ? b : a);
+    }
+    private void dfs(TreeNode n, List<Integer> l, int level) {
+        if (n == null) { return; } 
+        if (l.size() == level) { l.add(n.val); } // never reach this level before, add first value.
+        else { l.set(level, l.get(level) + n.val); } // reached the level before, accumulate current value to old value.
+        dfs(n.left, l, level + 1);
+        dfs(n.right, l, level + 1);
     }
 }
