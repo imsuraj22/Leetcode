2ba1524b088e750df39cd5a94fd118e4209ef82d
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-07-01 23:00:22 +0530

    Time: 1 ms (89.79%), Space: 41.4 MB (74.35%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -14,43 +14,36 @@
  * }
  */
 class Pair{
-        TreeNode root;
-        int level;
-        Pair(TreeNode root, int level){
-            this.root=root;
-            this.level=level;
-        }
-     }
+    TreeNode root;
+    int level;
+    Pair(TreeNode root, int level){
+        this.root=root;
+        this.level=level;
+    }
+ }
 class Solution {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        List<List<Integer>> result =new ArrayList<>();
-        if(root==null) return result;
-        Queue<Pair> q=new LinkedList<>();
+        List<List<Integer>> list=new ArrayList<>();
+    Queue<Pair> q=new LinkedList<>();
+    q.add(new Pair(root, 1));
+        if(root==null) return list;
+    while(!q.isEmpty()){
+        int size=q.size();
+        int l=0;
+        List<Integer> temp=new ArrayList<>();
+        for(int i=0;i<size;i++){
+            Pair curr=q.poll();
+            l=curr.level;
+            temp.add(curr.root.val);
+            if(curr.root.left!=null) q.add(new Pair(curr.root.left, l+1));
+            if(curr.root.right!=null) q.add(new Pair(curr.root.right, l+1));
 
-        int level=0;
-        q.add(new Pair(root, level+1));
-
-        while(!q.isEmpty()){
-
-            int size=q.size();
-            List<Integer> temp=new ArrayList<>();
-            int l=0;
-            for(int i=0;i<size;i++){
-                TreeNode curr=q.peek().root;
-                l=q.peek().level;
-                q.poll();
-                temp.add(curr.val);
-                if(curr.left!=null) q.add(new Pair(curr.left, l+1));
-                if(curr.right!=null) q.add(new Pair(curr.right, l+1));
-
-
-            }
-            if(l%2==0){
-                Collections.reverse(temp);
-            }
-            result.add(temp);
 
         }
-        return result;
+        if(l%2==0) Collections.reverse(temp);
+        list.add(temp);
+    }
+
+    return list;
     }
 }
