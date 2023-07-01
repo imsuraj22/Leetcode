35ef8738d6008ac73f34970f113dae19322a7c71
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-07-01 21:43:15 +0530

    Time: 1 ms (100.00%), Space: 43.6 MB (21.51%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -13,19 +13,41 @@
  *     }
  * }
  */
-class Solution {
-     private int max = 1;
-    public int widthOfBinaryTree(TreeNode root) {
-        if (root == null) return 0;
-        List<Integer> startOfLevel = new LinkedList<>();
-        helper(root, 0, 1, startOfLevel);
-        return max;
+class Pair{
+    TreeNode node;
+    int num;
+    Pair(TreeNode node, int num){
+        this.node=node;
+        this.num=num;
     }
-    public void helper(TreeNode root, int level, int index, List<Integer> list) {
-        if (root == null) return;
-        if (level == list.size()) list.add(index);
-        max = Math.max(max, index + 1 - list.get(level));
-        helper(root.left, level + 1, index * 2, list);
-        helper(root.right, level + 1, index * 2 + 1, list);
+ }
+class Solution {
+    public int widthOfBinaryTree(TreeNode root) {
+        if(root==null) return 0;
+        Queue<Pair> q=new LinkedList<>();
+        int ans=0;
+        q.add(new Pair(root, 0));
+        while(!q.isEmpty()){
+            int size=q.size();
+            int min=q.peek().num;
+            int fist=0, last=0;
+            for(int i=0;i<size;i++){
+                int curr_id=q.peek().num-min;
+                TreeNode curr=q.peek().node;
+                q.poll();
+
+                if(i==0) fist=curr_id;
+                if(i==size-1) last=curr_id;
+
+                if(curr.left!=null){
+                    q.add(new Pair(curr.left, curr_id*2+1));
+                }
+                if(curr.right!=null){
+                    q.add(new Pair(curr.right, curr_id*2+2));
+                }
+            }
+            ans=Math.max(ans, last-fist+1);
+        }
+        return ans;
     }
 }
