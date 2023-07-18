642539e74646a3ca0a6f7bdbe6914ceed4b04e4b
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-07-18 07:40:48 +0530

    Time: 1448 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -4,6 +4,8 @@ declare M INT;
 set M=N-1;
   RETURN (
       # Write your MySQL query statement below.
-      select distinct(salary) from employee order by salary desc limit M,1
+      select distinct(salary) from employee e1 
+      where n-1 = (select count(distinct salary) from employee e2 where e1.salary<e2.salary) 
+     
   );
 END
