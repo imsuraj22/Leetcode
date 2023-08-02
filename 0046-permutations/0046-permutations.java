f392a56aa7f673a37a9ad4d130712c1d4acf7343
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-08-02 21:40:53 +0530

    Time: 1 ms (98.58%), Space: 44.3 MB (12.89%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,21 +1,29 @@
 class Solution {
-    public List<List<Integer>> permute(int[] num) {
-    List<List<Integer>> ans = new ArrayList<List<Integer>>();
-    if (num.length ==0) return ans;
-    List<Integer> l0 = new ArrayList<Integer>();
-    l0.add(num[0]);
-    ans.add(l0);
-    for (int i = 1; i< num.length; ++i){
-        List<List<Integer>> new_ans = new ArrayList<List<Integer>>(); 
-        for (int j = 0; j<=i; ++j){            
-           for (List<Integer> l : ans){
-        	   List<Integer> new_l = new ArrayList<Integer>(l);
-        	   new_l.add(j,num[i]);
-        	   new_ans.add(new_l);
-           }
-        }
-        ans = new_ans;
+     public List<List<Integer>> permute(int[] nums) {
+
+        if (nums == null || nums.length == 0)
+            return new ArrayList<>();
+
+        List<List<Integer>> finalResult = new ArrayList<>();
+        permuteRecur(nums, finalResult, new ArrayList<>(), new boolean[nums.length]);
+        return finalResult;
     }
-    return ans;
-}
+
+    private void permuteRecur(int[] nums, List<List<Integer>> finalResult, List<Integer> currResult, boolean[] used) {
+
+        if (currResult.size() == nums.length) {
+            finalResult.add(new ArrayList<>(currResult));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i])
+                continue;
+            currResult.add(nums[i]);
+            used[i] = true;
+            permuteRecur(nums, finalResult, currResult, used);
+            used[i] = false;
+            currResult.remove(currResult.size() - 1);
+        }
+    }
 }
