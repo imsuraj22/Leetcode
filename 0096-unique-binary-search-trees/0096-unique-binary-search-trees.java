559afa4a5c723ba23df3585035e1a2b5102047cb
Suraj Gaikwad <100610668+imsuraj22@users.noreply.github.com>
2023-08-05 22:11:04 +0530

    Time: 0 ms (100.00%), Space: 39.9 MB (5.75%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -2,9 +2,12 @@ class Solution {
     public int numTrees(int n) {
         int dp[]=new int[n+1];
         Arrays.fill(dp,-1);
-        return solution(n,dp);
+        //return solution(n,dp);
+        return solutionTab(n);
     }
     
+    
+    //memoization solution
     int solution(int n, int dp[]){
         if(n<=1) return 1;
         if(dp[n]!=-1) return dp[n];
@@ -14,5 +17,20 @@ class Solution {
         }
         
         return dp[n]=ans;
+    }
+    
+    
+    //tabulation solution
+    int solutionTab(int n){
+        int dp[]=new int[n+1];
+        dp[0]=dp[1]=1;
+        
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        
+        return dp[n];
     }
 }
